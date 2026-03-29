b77ff7a079b3ce937aabf32d001f502157b13b21
DSP-V1 <kaiserhernandez692@gmail.com>
2026-03-29 16:52:33 -0600

    Update UIIcons-ForSectionBoxes.lua

--- UILib/icons/UIIcons-ForSectionBoxes.lua
+++ UILib/icons/UIIcons-ForSectionBoxes.lua
@@ -2,8 +2,10 @@ local IconsSB = {
     ["Person"] = "rbxassetid://17428322363",
     ["Settings"] = "rbxassetid://7059346386",
     ["Shop"] = "rbxassetid://11807308307",
-    ["Shop-2"] = "rbxassetid://9405933234"
-    ["Alert"] = "rbxassetid://17616650762"
+    ["Shop-2"] = "rbxassetid://9405933234",
+    ["Alert"] = "rbxassetid://17616650762",
+    ["Info"] = "rbxassetid://135453418174430",
+    ["White-Discord"] = "rbxassetid://110769239384778"
 }
 
 local function getIcon(name)
